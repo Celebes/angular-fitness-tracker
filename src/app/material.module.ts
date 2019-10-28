22bbfcf859d7dadc555653f8352aa9eb42775602
Krzysztof Gurniak <kris1337@gmail.com>
2019-10-28 09:51:18 +0100

    register and login forms

---
+++ src/app/material.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule {
+}
